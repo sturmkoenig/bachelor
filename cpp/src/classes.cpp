218b36963f4b048b410f51643dd058ae07db3e69
sturmkoenig <linuslauer@protonmail.com>
2019-04-15 14:30:26 +0200

    added phasesingularity counting

--- cpp/src/classes.cpp
+++ cpp/src/classes.cpp
@@ -354,6 +354,12 @@ void Heart_Simulation::simulation(const std::string visualization, const bool sp
             {
 		phase.make_phasemap(v, h);
 		PS.line_integral(phase);
+                num_ps = PS.count_singularitys();
+		if(num_ps == 0)
+		{
+		    std::cout << "great success\n"
+		    return;
+		}
 		// num_ps = PS.count_singularitys();
 		// check_if_terminated(num_ps, counter);
                 gp << "set title \"" << i << "\"\n";
