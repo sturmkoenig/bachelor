f2951a50b68ab8bba8308c1c8180c1e82457fef8
sturmkoenig <36859598+sturmkoenig@users.noreply.github.com>
2020-06-21 17:01:54 +0200

    Delete save_to_mpi.sh

--- cpp/save_to_mpi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-scp -r ./src lauer@login.lfpn.ds.mpg.de:/home/lauer/smaxxhome/Documents/Heart_Simulation/cpp/ 
-scp -r ./src lauer@login.lfpn.ds.mpg.de:/home/lauer/smaxxhome/Documents/Heart_Simulation/sirona/
-#scp -r ./../saved_states lauer@login.lfpn.ds.mpg.de:/home/lauer/smaxxhome/Documents/Heart_Simulation/
